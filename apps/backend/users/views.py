16b90ab318b58108de19b197effd310c1397d90b
ShivanshRaj <rajputsinghshiv17@gmail.com>
2026-03-16 11:00:00 +0530

    feat(auth): develop robust custom django user abstractions

--- apps/backend/users/views.py
+++ apps/backend/users/views.py
@@ -12,3 +12,10 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
